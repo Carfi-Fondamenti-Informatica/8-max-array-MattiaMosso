074ef9478ed791fdfb51b809d4fd08f5cf4370e3
MattiaMosso <91486653+MattiaMosso@users.noreply.github.com>
2021-12-11 09:36:13 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,15 @@
+//
+// Created by Mattia on 08/12/2021.
+//
 
+float max (float n[], int d){
+    float m=n[0];
+    int i;
+
+    for (i=1;i<d;i++){
+        if (n[i]>m){
+            m=n[i];
+        }
+    }
+    return m;
+}
