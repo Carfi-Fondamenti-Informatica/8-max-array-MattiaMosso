659144aa01997ec7031a634d42fade2dc1db495a
MattiaMosso <91486653+MattiaMosso@users.noreply.github.com>
2021-12-11 09:35:11 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,18 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
-int main(){
-  return 0;
+
+int main() {
+    int a;
+    float j;
+
+    cin >>a;
+    float n[a];
+    for (int i=0; i<a; i++){
+        cin >>j;
+        n[i]=j;
+    }
+    cout << max (n,a);
+    return 0;
 }
